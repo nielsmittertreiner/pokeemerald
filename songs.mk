1c7cde8900d7bb3272f2eb3456b63eb79ad871e3
Niels Mittertreiner <mittertreinerniels@gmail.com>
2021-04-18 13:13:19 +0200

    Update Murena City

--- songs.mk
+++ songs.mk
@@ -1274,3 +1274,6 @@ $(MID_SUBDIR)/mus_sunset.s: %.s: %.mid
 
 $(MID_SUBDIR)/mus_celebi.s: %.s: %.mid
 	$(MID) $< $@ -E -R$(STD_REVERB) -G195 -v100
+
+$(MID_SUBDIR)/mus_murena.s: %.s: %.mid
+	$(MID) $< $@ -E -R$(STD_REVERB) -G196 -v100
